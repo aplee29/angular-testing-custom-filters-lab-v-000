83903936135a21ce8ef2b581f1f9dc15aa97eeb8
Ann Lee <aplee29@users.noreply.github.com>
2017-01-29 21:39:35 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,33 @@
 describe('Favorite Food Filter', function () {
 	var $controller;
 
+	var people = [
+		{
+			name: 'Ann',
+			favoriteFood: 'sushi'
+		},
+		{
+			name: 'John',
+			favoriteFood: 'pizza'
+		},
+		{
+			name: 'Jane',
+			favoriteFood: 'tacos'
+		}
+	]
+
 	beforeEach(module('app'));
 
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	beforeEach(inject(function ($filter) {
+		favoriteFood = $filter('favoriteFood');
+	}));
+
+	it('should filter the list by favorite food', function() {
+		// expect(favoriteFood(people, 'tacos').length).toBe(1);
+		expect(favoriteFood(people, 'tacos').length).toEqual(1);
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,11 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
+	}));
+
+	it('should remove all vowels from a string', function() {
+		expect(removeAllVowels('javascript')).toEqual('jvscrpt');
+	});
 });
